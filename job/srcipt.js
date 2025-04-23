475829527907652996e6f44b34791697374af7ad
Vishal Gupta <147253066+vishal1238@users.noreply.github.com>
2025-04-23 22:31:57 +0530

    Delete job directory

--- job/srcipt.js
+++
@@ -1,4 +0,0 @@
-function showform(formId){
-    document.querySelectorAll(".form-box").forEach(form=> form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
